75c76115f82e3af13d641c184801cff9dc326d8b
Haim Braunstein <haim321@gmail.com>
2024-03-25 19:01:40 +0200

    Add route contact details

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -2,8 +2,8 @@ const Router = ReactRouterDOM.HashRouter
 const { Route, Routes } = ReactRouterDOM
 const { Provider } = ReactRedux
 
+import { ContactDetails } from './pages/ContactDetails.jsx'
 import { HomePage } from './pages/HomePage.jsx'
-
 export class App extends React.Component {
 
     render() {
@@ -15,7 +15,8 @@ export class App extends React.Component {
 
                     <main className="main-layout">
                         <Routes>
-                            <Route element={<HomePage />} path="/"></Route>
+                            <Route element={<HomePage />} path="/"/>
+                            <Route element={<ContactDetails />} path="/contact/:id"/>
                         </Routes>
                     </main>
 
