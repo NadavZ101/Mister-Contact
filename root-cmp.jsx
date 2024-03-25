0c5fe03fe92c6abda086da0bd780875360813f50
NadavZ101 <nadavzivyon@gmail.com>
2024-03-25 17:05:30 +0200

    Setup HomePage cmp in rootCmp

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,4 +1,29 @@
+const Router = ReactRouterDOM.HashRouter
+const { Route, Routes } = ReactRouterDOM
+const { Provider } = ReactRedux
 
-export function App() {
+import { HomePage } from './pages/HomePage.jsx'
 
+export class App extends React.Component {
+
+    render() {
+        return (
+            // <Provider store={store}>
+            <Router>
+                <section className="app">
+                    {/* <AppHeader /> */}
+
+                    <main className="main-layout">
+                        <Routes>
+                            <Route element={<HomePage />} path="/"></Route>
+                        </Routes>
+                    </main>
+
+                </section>
+            </Router>
+
+
+            // </Provider>
+        )
+    }
 }
